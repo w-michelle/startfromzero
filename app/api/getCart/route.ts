ca608fcc7c89173306fcff6bdb9afaee3b45ebd4
w-michelle <89858197+w-michelle@users.noreply.github.com>
2023-11-30 23:39:01 -0500

    fix getCart

--- app/api/getCart/route.ts
+++ app/api/getCart/route.ts
@@ -16,29 +16,33 @@ export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
   const cart = await findCart(session);
 
-  const products = await Promise.all(
-    cart?.cartItems.map(async (item) => {
-      const product = await prisma?.product.findFirst({
-        where: { id: item.productId },
-        include: { images: true },
-      });
+  if (cart) {
+    const products = await Promise.all(
+      cart?.cartItems.map(async (item) => {
+        const product = await prisma?.product.findFirst({
+          where: { id: item.productId },
+          include: { images: true },
+        });
 
-      if (product) {
-        for (let i = 0; i < product.images.length; i++) {
-          const params = {
-            Bucket: process.env.A_BUCKET_NAME,
-            Key: product.images[i].imageKey,
-          };
-          const command = new GetObjectCommand(params);
-          const url = await getSignedUrl(s3Client, command, {
-            expiresIn: 3600,
-          });
-          product.images[i].url = url;
+        if (product) {
+          for (let i = 0; i < product.images.length; i++) {
+            const params = {
+              Bucket: process.env.A_BUCKET_NAME,
+              Key: product.images[i].imageKey,
+            };
+            const command = new GetObjectCommand(params);
+            const url = await getSignedUrl(s3Client, command, {
+              expiresIn: 3600,
+            });
+            product.images[i].url = url;
+          }
         }
-      }
-      return product;
-    }) || []
-  );
+        return product;
+      }) || []
+    );
 
-  return NextResponse.json({ ...cart, products: products });
+    return NextResponse.json({ ...cart, products: products });
+  } else {
+    return new NextResponse("Cart Error", { status: 501 });
+  }
 }
