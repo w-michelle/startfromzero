234c37ff57a0bee3fbb7e9be7a4e01cce6831c8e
w-michelle <89858197+w-michelle@users.noreply.github.com>
2023-12-01 16:02:33 -0500

    img loading

---
+++ app/(routes)/renovation/loading.tsx
@@ -0,0 +1,19 @@
+const Loading = () => {
+  return (
+    <div className="max-w-[1200px] text-white flex flex-col items-center my-4 h-screen mx-20">
+      <p className="self-start mb-6 text-font text-sm font-bold tracking-widest">
+        ELEPHANT GROUNDS
+      </p>
+      <div className="scrollbar grid grid-cols-3 h-[270px] sm:h-[620px] lg:w-[930px] lg:h-[620px] overflow-y-scroll overflow-x-hidden gap-2">
+        {Array.from({ length: 12 }, (movie, i) => (
+          <div
+            key={i}
+            className="w-[90px] h-[90px] sm:w-[200px] sm:h-[200px] lg:w-[300px] lg:h-[300px] bg-gray-700/30 animate-pulse rounded-md"
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- app/(routes)/renovation/page.tsx
+++ app/(routes)/renovation/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { MdOutlineClose } from "react-icons/md";
+import Loading from "./loading";
+
 const Renovation = () => {
   const [selectedImg, setSelectedImg] = useState("");
   const [open, setOpen] = useState(false);
@@ -18,41 +20,42 @@ const Renovation = () => {
   }
 
   return (
-    <div className="max-w-[1200px] text-white flex flex-col items-center my-4 h-screen mx-20">
-      <p className="self-start mb-6 text-font text-sm font-bold tracking-widest">
-        ELEPHANT GROUNDS
-      </p>
-      <div className="scrollbar grid grid-cols-3 h-[270px] sm:h-[620px] lg:w-[930px] lg:h-[620px] overflow-y-scroll overflow-x-hidden gap-2">
-        {imgarr.map((item, index) => (
-          <div
-            key={index}
-            className="relative w-[90px] h-[90px] sm:w-[200px] sm:h-[200px] lg:w-[300px] lg:h-[300px]"
-          >
-            <Image
-              fill
-              src={item}
-              alt="elephant grind reno"
-              className="hover:scale-110 hover:cursor-pointer hover:z-[7777]"
-              onClick={() => bigImg(item)}
-            />
-          </div>
-        ))}
-      </div>
-
-      {open && (
-        <div className="z-[10000] absolute top-0 right-0 backdrop-blur-sm bg-black/40  w-full h-full flex justify-center pt-[120px]">
-          <button
-            className="absolute text-3xl top-20 right-20"
-            onClick={() => setOpen(false)}
-          >
-            <MdOutlineClose />
-          </button>
-          <div className="relative w-[300px] h-[300px] md:w-[500px] md:h-[500px]">
-            <Image fill src={selectedImg} alt="reno full image" />
-          </div>
+    <Suspense fallback={<Loading />}>
+      <div className="max-w-[1200px] text-white flex flex-col items-center my-4 h-screen mx-20">
+        <p className="self-start mb-6 text-font text-sm font-bold tracking-widest">
+          ELEPHANT GROUNDS
+        </p>
+        <div className="scrollbar grid grid-cols-3 h-[270px] sm:h-[620px] lg:w-[930px] lg:h-[620px] overflow-y-scroll overflow-x-hidden gap-2">
+          {imgarr.map((item, index) => (
+            <div
+              key={index}
+              className="relative w-[90px] h-[90px] sm:w-[200px] sm:h-[200px] lg:w-[300px] lg:h-[300px]"
+            >
+              <Image
+                fill
+                src={item}
+                alt="elephant grind reno"
+                className="hover:scale-110 hover:cursor-pointer hover:z-[7777]"
+                onClick={() => bigImg(item)}
+              />
+            </div>
+          ))}
         </div>
-      )}
-    </div>
+        {open && (
+          <div className="z-[10000] absolute top-0 right-0 backdrop-blur-sm bg-black/40  w-full h-full flex justify-center pt-[120px]">
+            <button
+              className="absolute text-3xl top-20 right-20"
+              onClick={() => setOpen(false)}
+            >
+              <MdOutlineClose />
+            </button>
+            <div className="relative w-[300px] h-[300px] md:w-[500px] md:h-[500px]">
+              <Image fill src={selectedImg} alt="reno full image" />
+            </div>
+          </div>
+        )}
+      </div>
+    </Suspense>
   );
 };
 
