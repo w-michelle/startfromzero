2e6060115f9ceed8a47b853351d9c82f8ced6d98
w-michelle <89858197+w-michelle@users.noreply.github.com>
2023-11-30 23:50:11 -0500

    fix getCart

--- app/api/getCart/route.ts
+++ app/api/getCart/route.ts
@@ -4,7 +4,7 @@ import { authOptions } from "../auth/[...nextauth]/route";
 import { findCart } from "../addToCartAction/route";
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
+import prisma from "@/lib/prismadb";
 const s3Client = new S3Client({
   region: "ca-central-1",
   credentials: {
@@ -16,33 +16,29 @@ export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
   const cart = await findCart(session);
 
-  if (cart) {
-    const products = await Promise.all(
-      cart?.cartItems.map(async (item) => {
-        const product = await prisma?.product.findFirst({
-          where: { id: item.productId },
-          include: { images: true },
-        });
+  const products = await Promise.all(
+    cart?.cartItems.map(async (item) => {
+      const product = await prisma?.product.findFirst({
+        where: { id: item.productId },
+        include: { images: true },
+      });
 
-        if (product) {
-          for (let i = 0; i < product.images.length; i++) {
-            const params = {
-              Bucket: process.env.A_BUCKET_NAME,
-              Key: product.images[i].imageKey,
-            };
-            const command = new GetObjectCommand(params);
-            const url = await getSignedUrl(s3Client, command, {
-              expiresIn: 3600,
-            });
-            product.images[i].url = url;
-          }
+      if (product) {
+        for (let i = 0; i < product.images.length; i++) {
+          const params = {
+            Bucket: process.env.A_BUCKET_NAME,
+            Key: product.images[i].imageKey,
+          };
+          const command = new GetObjectCommand(params);
+          const url = await getSignedUrl(s3Client, command, {
+            expiresIn: 3600,
+          });
+          product.images[i].url = url;
         }
-        return product;
-      }) || []
-    );
+      }
+      return product;
+    }) || []
+  );
 
-    return NextResponse.json({ ...cart, products: products });
-  } else {
-    return new NextResponse("Cart Error", { status: 501 });
-  }
+  return NextResponse.json({ ...cart, products: products });
 }
