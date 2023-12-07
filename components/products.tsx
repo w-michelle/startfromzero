41418bfe57dcf9201ce4ad9dc4b67b3f6e0de8bb
w-michelle <89858197+w-michelle@users.noreply.github.com>
2023-12-07 15:42:06 -0500

    fix img

--- components/products.tsx
+++ components/products.tsx
@@ -17,6 +17,7 @@ type Product = {
 };
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const timeStamp = new Date().getTime();
 
   useEffect(() => {
     const getProducts = async () => {
@@ -48,7 +49,11 @@ const Products = () => {
               }}
             >
               <div className="relative w-[200px] h-[200px]">
-                <Image fill src={product.images[0].url} alt={product.name} />
+                <Image
+                  fill
+                  src={`${product.images[0].url}?${timeStamp}`}
+                  alt={product.name}
+                />
               </div>
               <p>{product.name}</p>
               <p>{hkdollar.format(Number(product.price))}</p>
